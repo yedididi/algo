0316d9e9698352837a609e48cfaa866b50248b27
이예지 <yejilee@iyejis-MacBook-Air.local>
2025-01-30 17:08:19 +0900

    1080 ok

--- baekjoon/1080/code.cpp
+++ baekjoon/1080/code.cpp
@@ -12,19 +12,46 @@ void getInput(void)
 
     for (int i = 0; i < n; i++)
     {
+        string tmp;
+        cin >> tmp;
         for (int j = 0; j < m; j++)
-            cin >> mapOne[i][j]; 
+            mapOne[i][j] = tmp[j] - '0';
     }
 
     for (int i = 0; i < n; i++)
     {
+        string tmp;
+        cin >> tmp;
         for (int j = 0; j < m; j++)
-            cin >> mapOne[i][j]; 
+            mapTwo[i][j] = tmp[j] - '0';
     }
+}
+
+void flipNumbers(int startI, int startJ)
+{
+    for (int i = startI; i < startI + 3; i++)
+    {
+        for (int j = startJ; j < startJ + 3; j++)
+            mapOne[i][j] = !mapOne[i][j];
+    }
+}
+
+bool checkSame()
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (mapOne[i][j] != mapTwo[i][j])
+                return (false);
+        }
+    }
+    return (true);
 }
 
 int main(void)
 {
+    int flipCount = 0;
     getInput();
 
     for (int i = 0; i < n - 2; i++)
@@ -32,7 +59,14 @@ int main(void)
         for (int j = 0; j < m - 2; j++)
         {
             if (mapOne[i][j] != mapTwo[i][j])
+            {
                 flipNumbers(i, j);
+                flipCount++;
+            }
         }
     }
+    if (checkSame() == false)
+        cout << -1 << endl;
+    else
+        cout << flipCount << endl;
 }
