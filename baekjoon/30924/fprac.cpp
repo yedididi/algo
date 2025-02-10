dc500720226ddcd91437ba73ea9f8cb46d32195c
yedididi <iyeji366@gmail.com>
2025-02-10 18:28:14 +0900

    30924

--- baekjoon/30924/fprac.cpp
+++ baekjoon/30924/fprac.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstdlib>   // srand, rand
+#include <ctime>     // time
 
 using namespace std;
 
@@ -6,33 +10,37 @@ int main()
 {
     int respA;
 
+    srand(time(0));
+
+    vector<int> randNum(10000);
+    for (int i = 0; i < 10000; i++)
+        randNum[i] = i + 1;
+        
+    random_shuffle(randNum.begin(), randNum.end());
+
     for (int a = 1; a <= 10000; a++)
     {
-        std::cout << "? A " << a << std::endl;
-
-        // 채점기의 답변을 입력받는다.
+        std::cout << "? A " << randNum[a] << "\n" << std::flush;
         std::cin >> respA;
 
-        if(respA == 1 || (respA == 0 && a == 9999))
+        if(respA == 1)
         {
             int respB;
-
-            if (a == 9999)
-                a = 10000;
+            int ansA = randNum[a];
+            for (int i = 0; i < 10000; i++)
+                randNum[i] = i + 1;
+            random_shuffle(randNum.begin(), randNum.end());
 
             for (int b = 1; b <= 10000; b++)
             {
-                std::cout << "? B " << b << std::endl;
-        
-                // 채점기의 답변을 입력받는다.
+                std::cout << "? B " << randNum[b] << "\n" << std::flush;
                 std::cin >> respB;
             
-                if (respB == 1 || (respB == 0 && b == 9999))
+                if (respB == 1)
                 {
-                    if (b == 9999)
-                        b = 10000;
-                    std::cout << "! " << a + b << endl;
-                    break;
+                    int ansB = randNum[b];
+                    std::cout << "! " << ansA + ansB << endl;
+                    return (0);
                 }
             }
             break;
