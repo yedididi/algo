9e91204b50bb531d3e132785cb89a8c295a1e4d1
yedididi <iyeji366@gmail.com>
2025-02-18 13:10:00 +0900

    2231 ok

---
+++ baekjoon/2231/fprac.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+
+bool isValue(int input)
+{
+    int sum = 0;
+    int num = input;
+
+    while (num)
+    {
+        if (num / 10)
+        {
+            sum += (num % 10);
+            num = num / 10;
+        }
+        else
+        {
+            sum += num;
+            break;
+        }
+    }
+
+    if (n == (input + sum))
+        return (true);
+    return (false);
+}
+
+int main(void)
+{
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (isValue(i))
+        {
+            cout << i << endl;
+            return (0);
+        }
+    }
+    cout << 0 << endl;
+}
--- baekjoon/6987/prac.c
+++ baekjoon/6987/prac.c
@@ -3,18 +3,15 @@
 
 char *fstrcpy(char *str1, char *str2)
 {
-    char *returnStr = str1;
     int i = 0;
+    int strOneLen = strlen(str1);
 
-    while (*str2)
+    while (*(str2 + i))
     {
-        *(str1 + strlen(str1) + i) = *str2;
-        printf("%c\n", *(str1 + strlen(str1) + i));
+        *(str1 + strOneLen + i) = *(str2 + i);
         i++;
-        str2++;
     }
-    *(str1 + strlen(str1) + i) = '\0';
-    printf("%s\n", str1);
+    *(str1 + strOneLen + i) = '\0';
     return (str1);
 }
 
@@ -22,9 +19,6 @@ int main(void)
 {
     char str1[10] = "straw";
     char str2[10] = "berry";
-    char *finalStr;
-    
-    finalStr = fstrcpy(str1, str2);
 
-    printf("%s\n", finalStr);
+    printf("%s\n", fstrcpy(str1, str2));
 }
