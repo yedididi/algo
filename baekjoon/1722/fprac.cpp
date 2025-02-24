cd69ccc1737b1592a954f68515c96b4ae081a43f
yedididi <iyeji366@gmail.com>
2025-02-24 16:48:49 +0900

    before

--- baekjoon/1722/fprac.cpp
+++ baekjoon/1722/fprac.cpp
@@ -39,42 +39,9 @@ void printPermutation(int permutation[21], bool visited[21], int depth)
     }
 }
 
-bool isSame(int permutation[21])
-{
-    for (int i = 0; i < n; i++)
-    {
-        if (permutation[i] != arr[i])
-            return (false);
-    }
-    return (true);
-}
-
 void printWhichPermutation(int permutation[21], bool visited[21], int depth)
 {
-    if (endVal)
-        return ;
-
-    if (depth == n)
-    {
-        printTimes++;
-        if (isSame(permutation))
-        {
-            cout << printTimes << "\n";
-            // exit(0); //maybe not possible?
-            endVal = true;
-            return ;
-        }
-    }
-    for (int i = 1; i <= n; i++)
-    {
-        if (visited[i])
-            continue;
-        
-        visited[i] = true;
-        permutation[depth] = i;
-        printWhichPermutation(permutation, visited, depth + 1);
-        visited[i] = false;
-    }
+    
 }
 
 int main(void)
