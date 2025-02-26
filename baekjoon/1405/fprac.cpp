03bc474e4907d9cbe043303612745e316e4df77d
yedididi <iyeji366@gmail.com>
2025-02-26 18:53:11 +0900

    1405 middle

---
+++ baekjoon/1405/fprac.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+long double possibility[4];
+long double total = 0;
+
+int iNum[4] = {0, 0, -1, 1};
+int jNum[4] = {1, -1, 0, 0};
+
+void input(void)
+{
+    cin >> n;
+
+    for (int i = 0; i < 4; i++)
+    {
+        long double tmp;
+        cin >> tmp;
+        possibility[i] = tmp / 100.;
+    }
+}
+
+void dfs(int oldI, int oldJ, int depth, long double percentage, bool visited[40][40])
+{
+    // cout << "per is:" << percentage << endl;
+    if (depth == n)
+    {
+        // cout << percentage << endl;
+        total += percentage;
+        return ;
+    }
+
+    for (int index = 0; index < 4; index++)
+    {
+        int newI = oldI + iNum[index];
+        int newJ = oldJ + jNum[index];
+
+        if (visited[newI][newJ])
+            continue ;
+
+        visited[newI][newJ] = true;
+        dfs(newI, newJ, depth + 1, percentage * possibility[index], visited);
+        visited[newI][newJ] = false;
+    }
+}
+
+int main(void)
+{
+    bool visited[40][40] = {0, };
+
+    input();
+    dfs(20, 20, 0, 1., visited);
+    cout << total << endl;
+}
