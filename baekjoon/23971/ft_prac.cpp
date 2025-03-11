a2830ce77fbeabd4ba847485d76a705085702744
yedididi <iyeji366@gmail.com>
2025-03-11 10:49:14 +0900

    23971 ok

--- baekjoon/23971/ft_prac.cpp
+++ baekjoon/23971/ft_prac.cpp
@@ -2,6 +2,24 @@
 
 using namespace std;
 
+int h, w, n, m;
+
 int main(void)
 {
+    int total = 0;
+
+    cin >> h >> w >> n >> m;
+
+    if (h % (n + 1) == 0)
+        total = (h / (n + 1));
+    else
+        total = (h / (n + 1)) + 1;
+
+    
+    if (w % (m + 1) == 0)
+        total *= (w / (m + 1));
+    else
+        total *= ((w / (m + 1)) + 1);
+        
+    cout << total << endl;
 }
