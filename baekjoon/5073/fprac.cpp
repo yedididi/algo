840384b6224f0f6493d729e53a43eab352420fc9
yedididi <iyeji366@gmail.com>
2025-03-11 11:54:54 +0900

    5073 ok

--- baekjoon/5073/fprac.cpp
+++ baekjoon/5073/fprac.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main(void)
 {
-    
+    while (1)
+    {
+        vector<int> numbers(3, 0);
+
+        cin >> numbers[0] >> numbers[1] >> numbers[2];
+
+        if (numbers[0] == 0)
+            break;
+        
+        sort(numbers.begin(), numbers.end());
+        
+        if (numbers[2] >= (numbers[0] + numbers[1]))
+            cout << "Invalid\n";
+        else if (numbers[2] == numbers[0])
+            cout << "Equilateral\n";
+        else if (numbers[2] == numbers[1])
+            cout << "Isosceles\n";
+        else
+            cout << "Scalene\n";
+    }
 }
