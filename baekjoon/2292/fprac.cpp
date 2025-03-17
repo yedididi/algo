f2a784edc533be6a962ed227f1d754dd4dd33f84
yedididi <iyeji366@gmail.com>
2025-03-17 14:09:03 +0900

    2292 44per

--- baekjoon/2292/fprac.cpp
+++ baekjoon/2292/fprac.cpp
@@ -1,11 +1,42 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int n;
-
 int main(void)
 {
+    int n, level = 0, num = 0, a = 0, b = 0;
+    vector<int> web[7];
+
     cin >> n;
-    cout << n;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 1; j <= 6; j++)
+        {
+            if (level == 0)
+                num = 1;
+            else
+            {
+                num = *(--web[j].end()) + ((level - 1) * 6) + (j);
+                // cout << *(--web[j].end()) << " + " << ((level - 1) * 6) << " + " <<  (j);
+            }
+            web[j].push_back(num);
+            a = b;
+            b = num;
+            // cout << " = (" << a << ", " << b << ")\n"; 
+            if (num > n)
+                break;
+        }
+        if (num > n)
+            break;
+        level++;
+    }
+
+    if (num == 1)
+        cout << "1" << endl;
+    else if ((b - num) >= (num - a))
+        cout << (level + (num - a)) + 1 << endl;
+    else
+        cout << (level + (b - num)) + 1 << endl;
 }
