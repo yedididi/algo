2d9737903a74ea17e77c4fdd560964f678e53cb9
yedididi <iyeji366@gmail.com>
2025-04-24 14:44:09 +0900

    1927 ok

---
+++ baekjoon/1927/fprac.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int n;
+
+int main(void)
+{
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    multiset<int> ms;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        int tmp;
+
+        cin >> tmp;
+
+        if (tmp == 0)
+        {
+            if (ms.empty())
+                cout << "0\n";
+            else
+            {
+                auto value = ms.begin();
+                cout << *value << "\n";
+                ms.erase(value);
+            }
+        }
+        else
+            ms.insert(tmp);
+    }
+}
