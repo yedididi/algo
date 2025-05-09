7860b86f8ba6651839cd9419bdf3965bdd36879c
yedididi <iyeji366@gmail.com>
2025-05-09 11:49:19 +0900

    upup

--- baekjoon/16234/fprac.cpp
+++ baekjoon/16234/fprac.cpp
@@ -6,3 +6,12 @@ int main(void)
 {
     
 }
+
+typedef struct s_aboutMe
+{
+    std::string name = "이예지";
+    std::string github = "https://github.com/yedididi";
+    std::string velog = "https://velog.io/@yedididi/posts";
+    std::string phone = "010-8591-7245";
+    std::string email = "curry1115@naver.com";
+} t_aboutMe;
