b38998f32c7d2c69202364571849f90e2f28ee8d
yedididi <iyeji366@gmail.com>
2025-05-22 14:07:14 +0900

    5719 answer ok mem over

--- baekjoon/5719/fprac.cpp
+++ baekjoon/5719/fprac.cpp
@@ -12,10 +12,7 @@ typedef struct s_node
     int nodeNum;
     int sum;
 
-    s_node(int n, int s) : nodeNum(n), sum(s)
-    {
-
-    }
+    s_node(int n, int s) : nodeNum(n), sum(s) {}
 
     bool operator<(const s_node n) const
     {
@@ -30,57 +27,90 @@ typedef struct s_mapNode
     int distance;
 } t_mapNode;
 
+typedef struct s_history
+{
+    int nodeNum;
+    int sum;
+    vector<pair<int, int>> history;
+
+    s_history(int n, int s, vector<pair<int, int>> h = {}) : nodeNum(n), sum(s), history(h) {}
+
+    bool operator<(const s_history n) const
+    {
+        return (this->sum > n.sum);
+    }
+} t_history;
+
 int getAlmostMinDist(vector<vector<t_mapNode>> map)
 {
     priority_queue<t_node> q;
-    bool visited[endNum];
-
-    memset(visited, 0, endNum);
 
     t_node firstNode = {start, 0};
     q.push(firstNode);
 
-    // priority_queue<t_node> answers;
     while (!q.empty())
     {
         t_node poppedNode = q.top();
         q.pop();
 
-        cout << "nodeNum is " << poppedNode.nodeNum << endl;
+        // cout << "nodeNum is " << poppedNode.nodeNum << endl;
+        if (poppedNode.nodeNum == endNum)
+            return (poppedNode.sum);
+
+        for (int i = 0; i < map[poppedNode.nodeNum].size(); i++)
+        {
+            // cout << "map[" << poppedNode.nodeNum << "][" << i << "].distance is " << map[poppedNode.nodeNum][i].distance << endl;
+            if (map[poppedNode.nodeNum][i].distance == 1001)
+                continue;
+            t_node newNode = {map[poppedNode.nodeNum][i].nextNodeNum, map[poppedNode.nodeNum][i].distance + poppedNode.sum};
+            q.push(newNode);
+        }
+    }
+    return (-1);
+}
+
+int getMinDist(vector<vector<t_mapNode>> &map)
+{
+    int minValue = -1;
+    priority_queue<t_history> q;
+
+    t_history firstNode = {start, 0};
+    q.push(firstNode);
+
+    while (!q.empty())
+    {
+        t_history poppedNode = q.top();
+        q.pop();
+
+        // cout << "nodeNum is " << poppedNode.nodeNum << endl;
         if (poppedNode.nodeNum == endNum)
         {
-            cout << "reached endNum, dist is " << poppedNode.sum << endl;
-            if (almostMin == -1)
-                almostMin = poppedNode.sum;
-            else if (almostMin == poppedNode.sum)
-                ;
+            // cout << "reached endNum, dist is " << poppedNode.sum << endl;
+
+            if (minValue == -1)
+            {
+                // cout << "minValue is -1\n";
+                minValue = poppedNode.sum;
+                for (auto itr = poppedNode.history.begin(); itr != poppedNode.history.end(); itr++)
+                    map[itr->first][itr->second].distance = 1001;
+            }
+            else if (minValue == poppedNode.sum)
+            {
+                // cout << "minValue is " << minValue << endl;
+                for (auto itr = poppedNode.history.begin(); itr != poppedNode.history.end(); itr++)
+                    map[itr->first][itr->second].distance = 1001;
+            }
             else
-                return (poppedNode.sum);
-            // answers.push(poppedNode);
+                break;
         }
 
         for (int i = 0; i < map[poppedNode.nodeNum].size(); i++)
         {
-            // if (visited[map[poppedNode.nodeNum][i].nextNodeNum] == true)
-            //     continue;
-            t_node newNode = {map[poppedNode.nodeNum][i].nextNodeNum, map[poppedNode.nodeNum][i].distance + poppedNode.sum};
+            t_history newNode = {map[poppedNode.nodeNum][i].nextNodeNum, map[poppedNode.nodeNum][i].distance + poppedNode.sum, poppedNode.history};
+            newNode.history.push_back(make_pair(poppedNode.nodeNum, i));
             q.push(newNode);
-            // visited[map[poppedNode.nodeNum][i].nextNodeNum] = true;
         }
     }
-    
-    // while (!answers.empty())
-    // {
-    //     t_node poppedNode = answers.top();
-    //     answers.pop();
-
-    //     if (almostMin == -1)
-    //         almostMin = poppedNode.sum;
-    //     else if (almostMin == poppedNode.sum)
-    //         ;
-    //     else if (almostMin != poppedNode.sum)
-    //         return (poppedNode.sum);
-    // }
     return (-1);
 }
 
@@ -100,8 +130,8 @@ int main(void)
             int a, b, c;
             cin >> a >> b >> c;
             map[a].push_back({b, c});
-            // map[b].push_back({a, c});
         }
+        getMinDist(map);
         answer.push_back(getAlmostMinDist(map));
         almostMin = -1;
     }
