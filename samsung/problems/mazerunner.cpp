e2927b1b92d01338e3c896338d9d51df1fc9a9a5
yedididi <iyeji366@gmail.com>
2025-06-09 14:58:54 +0900

    input ok

--- samsung/problems/mazerunner.cpp
+++ samsung/problems/mazerunner.cpp
@@ -3,7 +3,39 @@
 
 using namespace std;
 
+int playerTotalCount;
+pair<int, int> exitCoordinate;
+vector<pair<int, int>> playerCoordinates;
+vector<vector<int>> map(11, vector<int>(11, 0));
+int totalMoveCount = 0;
+int n, k;
+
+void input(void)
+{
+    cin >> n >> playerTotalCount >> k;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+            cin >> map[i][j];
+    }
+
+    for (int i = 0; i < playerTotalCount; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        playerCoordinates.push_back(make_pair(a, b));
+        map[a][b] = 'P';
+    }
+
+    int a, b;
+    cin >> a >> b;
+    exitCoordinate.first = a;
+    exitCoordinate.second = b;
+    map[a][b] = 'E';
+}
+
 int main(void)
 {
-    
-}
+    input();
+}   
