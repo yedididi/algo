c98d67a944694b7d9e7040c8e3f705120586e212
yedididi <iyeji366@gmail.com>
2025-06-10 12:27:08 +0900

    ye first setting ok

--- samsung/problems/ye/fprac.cpp
+++ samsung/problems/ye/fprac.cpp
@@ -1,21 +1,45 @@
 //예술성
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+void input(void);
+
+int n = 0, sum = 0;
+vector<vector<int>>  map(30, vector<int>(30, 0));
+
+int getScore(void)
+{
+
+}
+
+void rotate(void)
+{
+
+}
+
 int main(void)
 {
-    for (int i = 0; i < squareSize; i++)
+    input();
+    
+    getScore();
+    for (int i = 0; i < 3; i++)
     {
-        for (int j = 0; j < squareSize; j++)
-        {
-            int oldI = startI + i;
-            int oldJ = startJ + j;
-            int newI = startI + j;
-            int newJ = startJ + (squareSize - 1) - i;
+        rotate();
+        getScore();
+    }
+    cout << sum << "\n";
+}
 
-            newMap[newI][newJ] = oldMap[oldI][oldJ];
+void input(void)
+{
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> map[i][j];
         }
     }
 }
-
